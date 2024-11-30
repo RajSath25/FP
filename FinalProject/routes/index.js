292ceebb59a6a20c74c928e8b4f6f9ad1ec456c3
Alex Unnippillil <alex.unnippillil@hotmail.com>
2024-11-29 19:09:28 -0500

    Update index.js  -+added get & post for register

--- FinalProject/routes/index.js
+++ FinalProject/routes/index.js
@@ -49,6 +49,38 @@ router.post("/login", function(req,res,next){
   }) (req,res,next)
 })
 
+
+// POST register page
+router.post('/register', function(req, res, next) {
+  let newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    displayName: req.body.displayName
+  });
+
+  User.register(newUser, req.body.password, (err) => {
+    if (err) {
+      console.log('Error: inserted New User');
+      if (err.name == 'UserExistsError') {
+        req.flash('registerMessage', 'Error: Username already taken!');
+        console.log('Error: User name taken!');
+      }
+      return res.render('Authentication/register', {
+        title: 'Register!!',
+        message: req.flash('registerMessage'),
+        displayName: req.user ? req.user.displayName : ''
+      });
+    } else {
+
+
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/');
+      });
+    }
+  });
+})
+
+
 router.get("/logout", (req,res,next)=>{
   req.logout(function(err){
     if (err)
